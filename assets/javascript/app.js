c89e0c35307107402502158284ae3d0c26b34886
Jamie ONeill <joneill@sembracare.com>
2019-09-27 18:03:32 -0400

    added backend functionality to be able to sort results by category......and dealt w/ being able to return entire array of each restaurant vs the one at [0]

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -47,14 +47,14 @@ $(document).ready(function () {
     }
 
     //On click event to pass the gps coordinates through the yelp AJAX request and get the yelp object
-    $('#btnYelpRequest').on('click', function() {
+    $('#btnYelpRequest').on('click', function () {
         // This is a workaround since the Yelp API does not accept CORS
         $.ajaxPrefilter(function (options) {
             if (options.crossDomain && jQuery.support.cors) {
                 options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
             }
         });
-        
+
         // AJAX Yelp API request
 
         $.ajax({
@@ -65,39 +65,53 @@ $(document).ready(function () {
             headers: {
                 "Authorization": "Bearer jCMmOYHc25KdfrfqfSx4_sejgdnb8eeYAprm_L8JTHaCtYPYVV9JFiG8xDZyCuwWq24y4vOZYOP8oCLUXgkO8XCGb2oh4-z54urrZYbKFxp5CPUeNxe42vi7DR2JXXYx",
             }
-        //promise that returns yelp object
+            //promise that returns yelp object
         }).then(function (response) {
             // console log to see the object returned
             console.log(response);
             console.log(response.businesses);
+            var localCategoryArr = [];
             for (let index = 0; index < response.businesses.length; index++) {
                 businessesArr.push(response.businesses[index]);
                 console.log(businessesArr);
             }
             for (let index = 0; index < businessesArr.length; index++) {
+                var dummyFilter = "American (Traditional)";
+                var categoryFilter = false;
+                var isFilter = true;
+                var restaurantCategoryArr = [];
                 console.log(index);
-                console.log("<image url" + businessesArr[index].image_url +">");
+                console.log("<image url" + businessesArr[index].image_url + ">");
                 //creating unordered list to dynamically add to HTML
                 var unorderedList = $("<ul class = 'collection'>");
                 //creating list item for each restaurant's data to dynamically add to HTML
                 var listItem = $("<li class = 'collection-item-avatar'>");
                 //creating restaurant image to dynamically add to HTML
                 var restaurantImg = $('<img src ="' + businessesArr[index].image_url + '" class = "circle">');
-                console.log(restaurantImg); 
+                console.log(restaurantImg);
                 //creating several <p>'s for restaurant info to dynamically add to HTML
                 //name of restaurant
                 var restaurantName = $("<p class = 'name'>").text(businessesArr[index].name);
-                console.log("name of restaurant " +restaurantName);
+                console.log("name of restaurant " + restaurantName);
                 //category of food 
-                console.log("category " + restaurantCategory); 
-                var restaurantCategory = $("<p class = 'name'>").text(businessesArr[index].categories[0].title);
-                categoryArr.push(businessesArr[index].categories[0].title); 
-                console.log("category " + restaurantCategory); 
+                console.log("category " + JSON.stringify(businessesArr[index].categories));
+                for (let i = 0; i < businessesArr[index].categories.length; i++) {
+                    console.log("businessesArr[index].categories[i].title" + businessesArr[index].categories[i].title);
+                    restaurantCategoryArr.push(businessesArr[index].categories[i].title);
+                    if (categoryArr.indexOf(businessesArr[index].categories[i].title) == -1) {
+                        categoryArr.push(businessesArr[index].categories[i].title);
+                    }
+                    if (businessesArr[index].categories[i].title === dummyFilter) {
+                        categoryFilter = true;
+                    }
+                }
+                console.log("restaurantCategoryArr = " + restaurantCategoryArr);
+                var restaurantCategory = $("<p class = 'name'>").text(restaurantCategoryArr);
                 //street address
                 console.log("street address " + businessesArr[index].location.address1);
                 var restaurantAddress = $("<p class = 'address'>").text(businessesArr[index].location.address1);
                 //phone number
-                console.log("phone number " +businessesArr[index].phone);
+                console.log("phone number " + businessesArr[index].phone);
                 var restaurantPhoneNumber = $("<p class = 'number'>").text(businessesArr[index].phone);
                 //star rating
                 console.log("rating " + businessesArr[index].rating);
@@ -109,24 +123,26 @@ $(document).ready(function () {
                 console.log("price " + businessesArr[index].price);
                 var restaurantPrice = $("<p class = 'price'>").text("Price: " + businessesArr[index].price);
                 //image url
+                if (categoryFilter || isFilter) {
+                    //appending what we just created to add to div in HTML next
+                    unorderedList.append(listItem);
+                    listItem.append(restaurantImg);
+                    listItem.append(restaurantName);
+                    listItem.append(restaurantCategory);
+                    listItem.append(restaurantAddress);
+                    listItem.append(restaurantPhoneNumber);
+                    listItem.append(restaurantRating);
+                    listItem.append(restaurantReviews);
+                    listItem.append(restaurantPrice);
 
-                //appending what we just created to add to div in HTML next
-                unorderedList.append(listItem);
-                listItem.append(restaurantImg);
-                listItem.append(restaurantName);
-                listItem.append(restaurantCategory);
-                listItem.append(restaurantAddress);
-                listItem.append(restaurantPhoneNumber);
-                listItem.append(restaurantRating);
-                listItem.append(restaurantReviews);
-                listItem.append(restaurantPrice);
+                    //prepending to div in HTML
+                    $("#yelp-info-storage").prepend(unorderedList);
+                }
 
-                //prepending to div in HTML
-                $("#yelp-info-storage").prepend(unorderedList);
 
             }
-            console.log("categoryArr" + categoryArr);
-            
+            console.log("categoryArr " + categoryArr);
+
         });
     });
 
